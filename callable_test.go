abd0b3719401d95e28a891b006f91c22be567c19
Martin Benda <martin@bendovi.cz>
2021-05-18 14:15:01 +0200

    callable_test.go: make tests compatible with Go 1.13

--- callable_test.go
+++ callable_test.go
@@ -10,6 +10,10 @@ import (
 	"testing"
 )
 
+func headerValues(res *http.Response, key string) []string {
+	return res.Header[http.CanonicalHeaderKey(key)]
+}
+
 type testRequest struct {
 	Who string `json:"who"`
 }
@@ -18,7 +22,7 @@ type testResponse struct {
 	Greeting string `json:"greeting"`
 }
 
-func (r *testRequest) Handle(ctx context.Context, call Call) (interface{}, error) {
+func (r *testRequest) Handle(_ context.Context, call Call) (interface{}, error) {
 	if r.Who == "" {
 		return nil, Error(NotFound, "nobody to greet")
 	}
@@ -38,10 +42,10 @@ func TestCallable_CORS(t *testing.T) {
 	res, body := response(w)
 
 	assert.Equal(t, 204, res.StatusCode)
-	vary := res.Header.Values("Vary")
+	vary := headerValues(res, "Vary")
 	assert.Contains(t, vary, "Origin")
 	assert.Contains(t, vary, "Access-Control-Request-Headers")
-	assert.Empty(t, res.Header.Values("Access-Control-Allow-Origin"))
+	assert.Empty(t, headerValues(res, "Access-Control-Allow-Origin"))
 	assert.Empty(t, body)
 }
 
@@ -53,8 +57,8 @@ func TestCallable_CORS_Origin(t *testing.T) {
 	res, body := response(w)
 
 	assert.Equal(t, 204, res.StatusCode)
-	assert.ElementsMatch(t, []string{"Origin", "Access-Control-Request-Headers"}, res.Header.Values("Vary"))
-	assert.Equal(t, []string{"http://localhost"}, res.Header.Values("Access-Control-Allow-Origin"))
+	assert.ElementsMatch(t, []string{"Origin", "Access-Control-Request-Headers"}, headerValues(res, "Vary"))
+	assert.Equal(t, []string{"http://localhost"}, headerValues(res, "Access-Control-Allow-Origin"))
 	assert.Empty(t, body)
 }
 
@@ -99,7 +103,7 @@ func TestCallable_Success(t *testing.T) {
 
 	assert.Equal(t, 200, res.StatusCode)
 	assert.Equal(t, "application/json; charset=utf-8", res.Header.Get("Content-Type"))
-	assert.Equal(t, []string{"Origin"}, res.Header.Values("Vary"))
+	assert.Equal(t, []string{"Origin"}, headerValues(res, "Vary"))
 	assert.JSONEq(t, `{"data":{"greeting":"Hello World!"}}`, body)
 }
 
@@ -112,7 +116,7 @@ func TestCallable_Error(t *testing.T) {
 
 	assert.Equal(t, 404, res.StatusCode)
 	assert.Equal(t, "application/json; charset=utf-8", res.Header.Get("Content-Type"))
-	assert.Equal(t, []string{"Origin"}, res.Header.Values("Vary"))
+	assert.Equal(t, []string{"Origin"}, headerValues(res, "Vary"))
 	assert.JSONEq(t, `{"error":{"status":"NOT_FOUND","message":"nobody to greet"}}`, body)
 }
 
@@ -125,7 +129,7 @@ func TestCallable_Error_Internal(t *testing.T) {
 
 	assert.Equal(t, 500, res.StatusCode)
 	assert.Equal(t, "application/json; charset=utf-8", res.Header.Get("Content-Type"))
-	assert.Equal(t, []string{"Origin"}, res.Header.Values("Vary"))
+	assert.Equal(t, []string{"Origin"}, headerValues(res, "Vary"))
 	assert.JSONEq(t, `{"error":{"status":"INTERNAL","message":"some error"}}`, body)
 }
 
@@ -139,6 +143,6 @@ func TestCallable_IID(t *testing.T) {
 
 	assert.Equal(t, 200, res.StatusCode)
 	assert.Equal(t, "application/json; charset=utf-8", res.Header.Get("Content-Type"))
-	assert.Equal(t, []string{"Origin"}, res.Header.Values("Vary"))
+	assert.Equal(t, []string{"Origin"}, headerValues(res, "Vary"))
 	assert.JSONEq(t, `{"data":{"greeting":"IID: 42"}}`, body)
 }
